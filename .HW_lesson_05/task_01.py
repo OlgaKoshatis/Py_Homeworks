94bb42259fd9c9dc5df015c7f709bf696a234bbe
olgaKoshatis <ahethyfy@gmail.com>
2023-07-02 17:04:24 +0300

    Исправлено решение 01 и 02 с исп рекурсии

--- .HW_lesson_05/task_01.py
+++ .HW_lesson_05/task_01.py
@@ -1,7 +1,12 @@
 #Напишите программу, которая на вход принимает два числа A и B, и возводит число А в целую степень B с помощью рекурсии.
 
 a, b = int(input("Введите число: ")), int(input("Введите степень: "))
-def degree(a):
-    x = a**b
-    return x
-print(degree(a))
+
+def degree(a, b):
+    if b == 0:
+        return 1 
+
+    return a * degree(a, b-1)
+
+print(degree(a, b))
+
