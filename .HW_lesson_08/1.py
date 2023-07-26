732f9734552621154abd56e8c302d04fdc1c5a98
olgaKoshatis <ahethyfy@gmail.com>
2023-07-26 10:48:39 +0300

    added 01,02 tasks

---
+++ .HW_lesson_08/1.py
@@ -0,0 +1,10 @@
+# import random
+# lst = ['robot'] * 10
+# lst += ['human'] * 10
+# random.shuffle(lst)
+# data = pd.DataFrame({'whoAmI'lst})
+# data.head() |
+
+import pandas as pd
+
+data = pd.DataFrame(random.sample(['black', 'white']*10, 20) ,columns={'Colors'})
