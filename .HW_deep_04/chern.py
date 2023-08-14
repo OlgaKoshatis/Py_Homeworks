207e9ad998507e3b96973d55bece09e42e525e0b
olgaKoshatis <ahethyfy@gmail.com>
2023-08-14 19:59:49 +0300

    task_01 & hard_task done

--- .HW_deep_04/chern.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-
-np.random.seed(20)
-
-my_list = [i for i in np.random.randint(1, 10, size=10)]
-print(my_list)
-my_stroke = ' '.join([str(i) for i in my_list])
-for i in range(len(my_list)):
-    if min(my_list)
